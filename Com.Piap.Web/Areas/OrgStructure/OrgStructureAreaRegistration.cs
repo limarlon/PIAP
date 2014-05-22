ec65cb622c2326bee528f09ef63076881f40d0cd
lidp <15098739840@163.com>
2014-05-22 14:52:17 +0800

    add dp

---
+++ Com.Piap.Web/Areas/OrgStructure/OrgStructureAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Com.Piap.Web.Areas.OrgStructure
+{
+    public class OrgStructureAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "OrgStructure";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "OrgStructure_default",
+                "OrgStructure/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
