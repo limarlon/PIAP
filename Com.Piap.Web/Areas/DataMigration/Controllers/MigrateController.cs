8caf9c62e71a8d2069c02a1adea8435e1c641a78
king0net <910012316@qq.com>
2014-05-23 17:19:57 +0800

    add upload

--- Com.Piap.Web/Areas/DataMigration/Controllers/MigrateController.cs
+++ Com.Piap.Web/Areas/DataMigration/Controllers/MigrateController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,8 +13,34 @@ namespace Com.Piap.Web.Areas.DataMigration.Controllers {
             return View();
         }
 
+        /// <summary>
+        /// GET html
+        /// </summary>
+        /// <returns></returns>
         public ActionResult DataImport() {
             return View();
         }
+
+        /// <summary>
+        /// 上传
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        public ActionResult Submit(IEnumerable<HttpPostedFileBase> files) {
+            foreach (var file in files) {
+                file.SaveAs(this.Server.MapPath("../../")+"/Upload/" + file.FileName);
+            }
+            return RedirectToAction("DataImport");
+        }
+        public ActionResult Result() {
+            return View();
+        }
+        private IEnumerable<string> GetFileInfo(IEnumerable<HttpPostedFileBase> files) {
+            return
+                from a in files
+                where a != null
+                select string.Format("{0} ({1} bytes)", Path.GetFileName(a.FileName), a.ContentLength);
+        }
+
     }
 }
